f99c8566c88b119e2f25192954d64c89097b8034
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-03-29 17:40:08 -0700

    AI Game Developer update

--- Installer/Assets/com.IvanMurzak/AI Particle System Installer/Installer.cs
+++ Installer/Assets/com.IvanMurzak/AI Particle System Installer/Installer.cs
@@ -19,7 +19,7 @@ namespace com.IvanMurzak.Unity.MCP.ParticleSystem.Installer
     public static partial class Installer
     {
         public const string PackageId = "com.ivanmurzak.unity.mcp.particlesystem";
-        public const string Version = "1.0.46";
+        public const string Version = "1.0.47";
 
         static Installer()
         {
